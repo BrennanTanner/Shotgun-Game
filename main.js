d2e5e670d3573d6f6edf3be95e429ed01e68bf4f
ncclapyohands <nathancook648@gmail.com>
2023-05-25 13:33:05 -0600

    commit

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-import Phaser from 'phaser';
-import PlayGame from './src/scenes/PlayGame.mjs';
+import Phaser from "phaser";
+import PlayGame from "./src/scenes/PlayGame.mjs";
+
+const screenHeight = screen.availHeight - 200;
+const screenWidth = screen.availWidth;
 
 var config = {
-   type: Phaser.AUTO,
-   width: 800,
-   height: 600,
-   physics: {
-      default: 'arcade',
-      arcade: {
-         debug: true,
-         gravity: { y: 300 },
-      },
-   },
-   scene: [PlayGame],
+  type: Phaser.AUTO,
+  width: screenWidth,
+  height: screenHeight,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+      gravity: { y: 300 },
+    },
+  },
+  scene: [PlayGame],
 };
 
 var game = new Phaser.Game(config);
