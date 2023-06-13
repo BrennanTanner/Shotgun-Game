b8fb4bb891a559ad58e6e0007750cd2ed9faf23c
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-06-13 13:42:38 -0600

    Kevin bullets wip

--- src/entities/player.mjs
+++ src/entities/player.mjs
@@ -1,86 +1,101 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
-   constructor(scene, x, y) {
-      super(scene, x, y, 'dude');
-      this.time = 0;
+  constructor(scene, x, y) {
+    super(scene, x, y, "dude");
+    this.time = 0;
 
-      //the player is made up of 2 seperate entities, the arm and the chair
-      this.arm = scene.physics.add.sprite(x, y, 'dude').setSize(48, 48);
-      this.chair = scene.physics.add.sprite(x, y, 'dude');
-   
-      scene.cameras.main.startFollow(this.arm, true);
-      //set the scene
-      this.scene = scene;
+    //the player is made up of 2 seperate entities, the arm and the chair
+    this.arm = scene.physics.add.sprite(x, y, "dude").setSize(48, 48);
+    this.chair = scene.physics.add.sprite(x, y, "dude");
 
-      // render player arm (offests it so it rotates correctly on the body)
-      this.arm.setCollideWorldBounds(true);
-      this.arm.setBounce(.4);
-      this.arm.setOffset(5, 10);
-      this.arm.setOrigin(0.3, 0.5);
+    //  Kevin's bullets
+    this.bullet = scene.physics.add.sprite(x, y, "bullet");
 
-      //define player animations
-      scene.anims.create({
-         key: 'arm',
-         frames: scene.anims.generateFrameNumbers('dude', { start: 1 }),
-      });
+    scene.cameras.main.startFollow(this.arm, true);
+    //set the scene
+    this.scene = scene;
 
-      scene.anims.create({
-         key: 'chair',
-         frames: scene.anims.generateFrameNumbers('dude', { start: 0 }),
-      });
+    // render player arm (offests it so it rotates correctly on the body)
+    this.arm.setCollideWorldBounds(true);
+    this.arm.setBounce(0.4);
+    this.arm.setOffset(5, 10);
+    this.arm.setOrigin(0.3, 0.5);
 
-      // add collison detection
-      scene.physics.add.collider(this.arm, scene.platforms);
+    //define player animations
+    scene.anims.create({
+      key: "arm",
+      frames: scene.anims.generateFrameNumbers("dude", { start: 1 }),
+    });
 
-      this.cursors = scene.input.keyboard.createCursorKeys();
-      this.activePointer = scene.input.activePointer;
-   }
+    scene.anims.create({
+      key: "chair",
+      frames: scene.anims.generateFrameNumbers("dude", { start: 0 }),
+    });
 
-   update() {
-      // define keyboard controls
-      this.pointerMove(this.activePointer);
+    scene.anims.create({
+      key: "bullet",
+      frames: scene.anims.generateFrameNumbers("bullet", { start: 0, end: 4 }),
+    });
 
-      //load player arm
-      this.arm.anims.play('arm');
+    // add collison detection
+    scene.physics.add.collider(this.arm, scene.platforms);
 
-      //load player chair
-      this.chair.anims.play('chair');
-      this.chair.setVelocityY(0);
-      this.chair.body.x = this.arm.body.x;
-      this.chair.body.y = this.arm.body.y;
+    this.cursors = scene.input.keyboard.createCursorKeys();
+    this.activePointer = scene.input.activePointer;
+  }
 
-      //decay velocity when touching the ground
-      if (this.arm.body.touching.down) {
-         if (this.arm.body.velocity.x > 10) {
-            this.arm.setVelocityX(this.arm.body.velocity.x - 10);
-          } else if (this.arm.body.velocity.x < -10) {
-            this.arm.setVelocityX(this.arm.body.velocity.x + 10);
-          } else if (this.arm.body.velocity.x < 10 && this.arm.body.velocity.x > -10){
-            this.arm.setVelocityX(0);
-          }
-          
+  update() {
+    // define keyboard controls
+    this.pointerMove(this.activePointer);
+
+    //load player arm
+    this.arm.anims.play("arm");
+
+    //load player chair
+    this.chair.anims.play("chair");
+    this.chair.setVelocityY(0);
+    this.chair.body.x = this.arm.body.x;
+    this.chair.body.y = this.arm.body.y;
+
+    this.bullet.anims.play("bullet");
+
+    //decay velocity when touching the ground
+    if (this.arm.body.touching.down) {
+      if (this.arm.body.velocity.x > 10) {
+        this.arm.setVelocityX(this.arm.body.velocity.x - 10);
+      } else if (this.arm.body.velocity.x < -10) {
+        this.arm.setVelocityX(this.arm.body.velocity.x + 10);
+      } else if (
+        this.arm.body.velocity.x < 10 &&
+        this.arm.body.velocity.x > -10
+      ) {
+        this.arm.setVelocityX(0);
       }
-   }
+    }
+  }
 
-   //set arm to pointer, when clicked set velocity to angle
-   pointerMove(pointer) {
-      var angleToPointer = Phaser.Math.Angle.Between(
-         this.arm.x,
-         this.arm.y,
-         pointer.worldX,
-         pointer.worldY
+  //set arm to pointer, when clicked set velocity to angle
+  pointerMove(pointer) {
+    var angleToPointer = Phaser.Math.Angle.Between(
+      this.arm.x,
+      this.arm.y,
+      pointer.worldX,
+      pointer.worldY
+    );
+    this.arm.rotation = angleToPointer;
+    this.time++;
+    if (this.activePointer.isDown && this.time > 20) {
+      const angle = this.scene.physics.velocityFromRotation(angleToPointer);
+      this.arm.setVelocity(
+        angle.x * 5 * -1 + this.arm.body.velocity.x,
+        angle.y * 5 * -1 + this.arm.body.velocity.y
       );
-      this.arm.rotation = angleToPointer;
-      this.time++;
-      if (this.activePointer.isDown && this.time > 20) {
-         const angle = this.scene.physics.velocityFromRotation(angleToPointer);
-         this.arm.setVelocity((angle.x * 5 * -1) + this.arm.body.velocity.x, (angle.y * 5 * -1) + this.arm.body.velocity.y);
-         this.time = 0;
-      }
-   }
+      this.time = 0;
+    }
+  }
 
-   hitEnemy(){
-      console.log(here);
-   }
+  hitEnemy() {
+    console.log(here);
+  }
 }
 
 export default Player;
--- src/scenes/PlayGame.mjs
+++ src/scenes/PlayGame.mjs
@@ -1,121 +1,120 @@
-import Player from '../entities/player.mjs';
-import Enemy from '../entities/enemy.mjs';
+import Player from "../entities/player.mjs";
+import Enemy from "../entities/enemy.mjs";
 
 class PlayGame extends Phaser.Scene {
-   constructor() {
-      super('playGame');
-      // declare objects
-      this.player;
-      this.platforms;
-      this.enemies;
-      // this.timedEvent = this.time.delayedCall(3000, this.onEvent, [], this);
-   }
+  constructor() {
+    super("playGame");
+    // declare objects
+    this.player;
+    this.platforms;
+    this.enemies;
+    // this.timedEvent = this.time.delayedCall(3000, this.onEvent, [], this);
+  }
 
-   preload() {
-      //get canvas
-      this.canvas = this.sys.game.canvas;
+  preload() {
+    //get canvas
+    this.canvas = this.sys.game.canvas;
 
-      // load assets
+    // load assets
 
-      //tiles
-      this.load.spritesheet({
-         key: 'city',
-         url: '../../res/images/city-tiles.png',
-         frameConfig: {
-            frameWidth: 16,
-            frameHeight: 16,
-         },
-      });
-      //player
-      this.load.spritesheet('dude', '../../res/player/Wheelchair-02.png', {
-         frameWidth: 48,
-         frameHeight: 48,
-      });
-      //enemy
-      this.load.spritesheet(
-         'chicken',
-         '../../res/enemy/Chicken/Idle (32x34).png',
-         {
-            frameWidth: 32,
-            frameHeight: 34,
-         }
-      );
-   }
-
-   create() {
-      //set w and h from canvas
-      let { width, height } = this.canvas;
-
-      // camera
-      this.cameras.main.setBounds(0, 0, height * 2, width * 2);
-
-      //background
-
-      //create groups
-      this.platforms = this.physics.add.staticGroup();
-
-      this.player = new Player(this, 100, 450);
-      console.log(this.player)
-      this.player.body = this.physics.add.body(this.player);
-      
-      this.enemies = this.add.group({
-         classType: Enemy,
-         maxSize: 10,
-         runChildUpdate: true,
-      });
-
-      this.cameras.main.setZoom(1.3);
-
-      //render level
-      for (let i = 8; i < width; i += 16) {
-         this.platforms.create(i, height - 24, 'city', '46');
+    //tiles
+    this.load.spritesheet({
+      key: "city",
+      url: "../../res/images/city-tiles.png",
+      frameConfig: {
+        frameWidth: 16,
+        frameHeight: 16,
+      },
+    });
+    //player
+    this.load.spritesheet("dude", "../../res/player/Wheelchair-02.png", {
+      frameWidth: 48,
+      frameHeight: 48,
+    });
+    //enemy
+    this.load.spritesheet(
+      "chicken",
+      "../../res/enemy/Chicken/Idle (32x34).png",
+      {
+        frameWidth: 32,
+        frameHeight: 34,
       }
+    );
+    // bullets
+    this.load.spritesheet(
+      "bullet",
+      "../../res/bullet/All_Fire_Bullet_Pixel_16x16.png",
+      {
+        frameWidth: 16,
+        frameHeight: 16,
+      }
+    );
+  }
 
-      this.platforms.create(600, 400, 'city', '26');
-      this.platforms.create(616, 400, 'city', '27');
-      this.platforms.create(632, 400, 'city', '28');
+  create() {
+    //set w and h from canvas
+    let { width, height } = this.canvas;
 
-      this.platforms.create(400, 200, 'city', '26');
-      this.platforms.create(416, 200, 'city', '27');
-      this.platforms.create(432, 200, 'city', '28');
+    // camera
+    this.cameras.main.setBounds(0, 0, height * 2, width * 2);
 
-      this.platforms.create(400, 500, 'city', '26');
-      this.platforms.create(416, 500, 'city', '27');
-      this.platforms.create(432, 500, 'city', '28');
+    //background
 
-      this.platforms.create(300, 400, 'city', '26');
-      this.platforms.create(316, 400, 'city', '27');
-      this.platforms.create(332, 400, 'city', '28');
+    //create groups
+    this.platforms = this.physics.add.staticGroup();
 
-      this.platforms.create(50, 250, 'city', '26');
-      this.platforms.create(66, 250, 'city', '27');
-      this.platforms.create(82, 250, 'city', '28');
+    this.player = new Player(this, 100, 450);
+    console.log(this.player);
+    this.player.body = this.physics.add.body(this.player);
 
-      this.platforms.create(750, 220, 'city', '26');
-      this.platforms.create(766, 220, 'city', '27');
-      this.platforms.create(782, 220, 'city', '28');
+    this.enemies = this.add.group({
+      classType: Enemy,
+      maxSize: 10,
+      runChildUpdate: true,
+    });
 
-      this.timedEvent = this.time.delayedCall(
-         3000,
-         this.spawnChicken,
-         [],
-         this
-      );
-      this.timedEvent = this.time.delayedCall(
-         1000,
-         this.spawnChicken,
-         [],
-         this
-      );
-   }
+    this.cameras.main.setZoom(1.3);
 
-   spawnChicken() {
-      this.enemies.create(500, 420);
-   }
+    //render level
+    for (let i = 8; i < width; i += 16) {
+      this.platforms.create(i, height - 24, "city", "46");
+    }
 
-   update() {
-      this.player.update();
-   }
+    this.platforms.create(600, 400, "city", "26");
+    this.platforms.create(616, 400, "city", "27");
+    this.platforms.create(632, 400, "city", "28");
+
+    this.platforms.create(400, 200, "city", "26");
+    this.platforms.create(416, 200, "city", "27");
+    this.platforms.create(432, 200, "city", "28");
+
+    this.platforms.create(400, 500, "city", "26");
+    this.platforms.create(416, 500, "city", "27");
+    this.platforms.create(432, 500, "city", "28");
+
+    this.platforms.create(300, 400, "city", "26");
+    this.platforms.create(316, 400, "city", "27");
+    this.platforms.create(332, 400, "city", "28");
+
+    this.platforms.create(50, 250, "city", "26");
+    this.platforms.create(66, 250, "city", "27");
+    this.platforms.create(82, 250, "city", "28");
+
+    this.platforms.create(750, 220, "city", "26");
+    this.platforms.create(766, 220, "city", "27");
+    this.platforms.create(782, 220, "city", "28");
+
+    this.timedEvent = this.time.delayedCall(3000, this.spawnChicken, [], this);
+    this.timedEvent = this.time.delayedCall(1000, this.spawnChicken, [], this);
+  }
+
+  spawnChicken() {
+    this.enemies.create(500, 420);
+  }
+
+  update() {
+    this.player.update();
+  }
 }
 
 export default PlayGame;
