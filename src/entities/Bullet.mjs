ccf09a91ba5d9634a5351a6fb6e493e41e718ebc
BrennanTanner <brenntanner@gmail.com>
2023-07-04 13:37:45 -0600

    formatting

--- src/entities/Bullet.mjs
+++ src/entities/Bullet.mjs
@@ -1,16 +1,16 @@
 class Bullet extends Phaser.Physics.Arcade.Sprite {
    constructor(scene, x, y) {
       super(scene, x, y, 'bullet');
-      
+
       this.bullet = this.scene.physics.add.existing(this).setCircle(10);
 
       // render bullet
       this.bullet.setCollideWorldBounds(true);
       this.bullet.setBounce(1);
       this.bullet.body.setAllowGravity(false);
-      this.bullet.mass = .2;
+      this.bullet.mass = 0.2;
       this.bullet.radius = 5;
-      this.bullet.growth = .5;
+      this.bullet.growth = 0.5;
       this.bullet.range = 20;
       //this.bullet.setOrigin(-2, 2);
 
@@ -27,33 +27,32 @@ class Bullet extends Phaser.Physics.Arcade.Sprite {
    update() {
       this.bullet.radius += this.bullet.growth;
       this.bullet.setCircle(this.bullet.radius);
-         //console.log(this.bullet);
+      //console.log(this.bullet);
 
       this.bullet.range--;
 
       //decay velocity when touching the ground
       if (this.bullet.body.touching.up) {
-         this.bullet.destroy()
-
+         this.bullet.destroy();
       }
 
-      if(this.bullet.range <= 0){
+      if (this.bullet.range <= 0) {
          this.bullet.destroy();
       }
    }
 
-  enemyCollision() {
-    console.log('hit');
-    if (localStorage.getItem('killCount')) {
-      // If it exists, increment the count by 1
-      const currentCount = parseInt(localStorage.getItem('killCount'));
-      const newCount = currentCount + 1;
-      localStorage.setItem('killCount', newCount);
-    } else {
-      // If it doesn't exist, create it and set it to 1
-      localStorage.setItem('killCount', 1);
-    }
-  }
+   enemyCollision() {
+      console.log('hit');
+      if (localStorage.getItem('killCount')) {
+         // If it exists, increment the count by 1
+         const currentCount = parseInt(localStorage.getItem('killCount'));
+         const newCount = currentCount + 1;
+         localStorage.setItem('killCount', newCount);
+      } else {
+         // If it doesn't exist, create it and set it to 1
+         localStorage.setItem('killCount', 1);
+      }
+   }
 }
 
 export default Bullet;
